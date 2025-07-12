1fe1f1957c615b0ae0e0134921ab932967e6d057
Antony Ibrahim <97265621+AntonyI1@users.noreply.github.com>
2025-07-12 16:53:56 -0700

    Typo in register size

--- src/vm.hpp
+++ src/vm.hpp
@@ -18,7 +18,7 @@ public:
 
 
 private:
-    std::array<uint8_t, 4> registers; // Four 32-bit registers: R0-R3
+    std::array<uint8_t, 4> registers; // Four 8-bit registers: R0-R3
     std::vector<uint8_t> memory; // Holds loaded program
     std::array<int32_t, 1024> memory_array; // Fixed memory block for LOAD/STORE
     std::stack<int32_t> stack; // LIFO stack for PUSH/POP
